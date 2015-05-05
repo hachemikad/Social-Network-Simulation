420487dfe96eca26971947f2107878e6009934b7
altear <telfer006@gmail.com>
2015-05-05 15:30:38 -0400

    Update: Python 3 Compatibility
    - Introduced error checking for deprecated function calls
    - Forced user inputs to be read back as integers.

--- generate_input.py
+++ generate_input.py
@@ -20,6 +20,7 @@ docs = input()
 print("""For how many ticks do you want the simulation to run?
 (remember there is a switch in the GUI called stop, turn in off if you want the simulation to run freely)""")
 ticks = input()
+print(type(ticks))
 print("""Choose the number of maximum likes for each peer at the start?
 (preferably proportional to the number of peers and documents in the network)""")
 likes = input()
@@ -35,12 +36,20 @@ outfile = open('set_variables.nls', 'w')
 replacements = {'num-peers ':numPeers, 'num-docs':numDocs, 'maxTicks 2000':maxTicks, 'max-docs-stored 3':maxLikes}  #using a dictionary for storing the variables to update the file
 
 for line in infile:
-    for src, target in replacements.iteritems():
-        line = line.replace(src, target)
-    outfile.write(line)
+    try:
+        for src, target in replacements.iteritems():
+            line = line.replace(src, target)
+        outfile.write(line)
+    except:
+        try:
+            for src, target in replacements.items():
+                line = line.replace(src, target)
+            outfile.write(line)
+        except:
+            print("incompatible version - apologies for hack code")
 
 infile.close()
 outfile.close()
 if ticks == '':
     ticks == 2000
-print("You now have a network of %d peers and %d documents, the simulation will run for %d ticks" % (size, docs, ticks))
+print("You now have a network of %d peers and %d documents, the simulation will run for %d ticks" % (int(size), int(docs), int(ticks)))
